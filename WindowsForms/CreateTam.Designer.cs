3a66e1cb38f68e6017d94550dd048cdd99e59e75
chpeel <1qwerty111222@mail.ru>
2017-12-10 23:30:00 +0300

    Add files via upload

---
+++ WindowsForms/CreateTam.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Tamagotchi
+{
+    partial class CreateTam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateTam));
+            this.textBoxNameTam = new System.Windows.Forms.TextBox();
+            this.labelNameTam = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonWhileBear = new System.Windows.Forms.Button();
+            this.buttonBeerPanda = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNameTam
+            // 
+            this.textBoxNameTam.Location = new System.Drawing.Point(402, 38);
+            this.textBoxNameTam.Multiline = true;
+            this.textBoxNameTam.Name = "textBoxNameTam";
+            this.textBoxNameTam.Size = new System.Drawing.Size(199, 35);
+            this.textBoxNameTam.TabIndex = 0;
+            // 
+            // labelNameTam
+            // 
+            this.labelNameTam.AutoSize = true;
+            this.labelNameTam.BackColor = System.Drawing.Color.Transparent;
+            this.labelNameTam.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelNameTam.Location = new System.Drawing.Point(25, 38);
+            this.labelNameTam.Name = "labelNameTam";
+            this.labelNameTam.Size = new System.Drawing.Size(282, 31);
+            this.labelNameTam.TabIndex = 1;
+            this.labelNameTam.Text = "1.Назовите медведя!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 58);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "2.Выберите медведя, нажав на его.\r\nПосле выбора медведя пути назад нет!";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(55, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(306, 421);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonWhileBear
+            // 
+            this.buttonWhileBear.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonWhileBear.AutoSize = true;
+            this.buttonWhileBear.BackColor = System.Drawing.Color.Transparent;
+            this.buttonWhileBear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonWhileBear.FlatAppearance.BorderSize = 0;
+            this.buttonWhileBear.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonWhileBear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttonWhileBear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonWhileBear.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonWhileBear.Image = ((System.Drawing.Image)(resources.GetObject("buttonWhileBear.Image")));
+            this.buttonWhileBear.Location = new System.Drawing.Point(409, 155);
+            this.buttonWhileBear.Name = "buttonWhileBear";
+            this.buttonWhileBear.Size = new System.Drawing.Size(248, 436);
+            this.buttonWhileBear.TabIndex = 8;
+            this.buttonWhileBear.UseVisualStyleBackColor = false;
+            this.buttonWhileBear.Click += new System.EventHandler(this.buttonWhileBear_Click);
+            // 
+            // buttonBeerPanda
+            // 
+            this.buttonBeerPanda.AutoSize = true;
+            this.buttonBeerPanda.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonBeerPanda.BackColor = System.Drawing.Color.Transparent;
+            this.buttonBeerPanda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonBeerPanda.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.buttonBeerPanda.FlatAppearance.BorderSize = 0;
+            this.buttonBeerPanda.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonBeerPanda.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttonBeerPanda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBeerPanda.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonBeerPanda.Image = ((System.Drawing.Image)(resources.GetObject("buttonBeerPanda.Image")));
+            this.buttonBeerPanda.Location = new System.Drawing.Point(684, 164);
+            this.buttonBeerPanda.Name = "buttonBeerPanda";
+            this.buttonBeerPanda.Size = new System.Drawing.Size(256, 436);
+            this.buttonBeerPanda.TabIndex = 9;
+            this.buttonBeerPanda.UseVisualStyleBackColor = false;
+            this.buttonBeerPanda.Click += new System.EventHandler(this.buttonBeerPanda_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(409, 597);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 81);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // CreateTam
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1280, 690);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonBeerPanda);
+            this.Controls.Add(this.buttonWhileBear);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelNameTam);
+            this.Controls.Add(this.textBoxNameTam);
+            this.Name = "CreateTam";
+            this.Text = "CreateTam";
+            this.Load += new System.EventHandler(this.CreateTam_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNameTam;
+        private System.Windows.Forms.Label labelNameTam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonWhileBear;
+        private System.Windows.Forms.Button buttonBeerPanda;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WindowsForms/CreateTam.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Tamagotchi
+{
+    public partial class CreateTam : Form
+    {
+        String typeBear = "";
+        public GamePoligon poligon;
+
+        public CreateTam()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CreateTam_Load(object sender, EventArgs e)
+        {
+            SoundPlayer sound = new SoundPlayer(@"..\..\..\s1.wav");
+            sound.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            typeBear = "Grizz";
+            MessageBox.Show("Самый лучший мишка!Выбор сделан!", "Сообщение");
+        }
+
+        private void buttonWhileBear_Click(object sender, EventArgs e)
+        {
+            typeBear = "While";
+            MessageBox.Show("Самый лучший мишка!Выбор сделан!", "Сообщение");
+        }
+
+        private void buttonBeerPanda_Click(object sender, EventArgs e)
+        {
+            typeBear = "Panda";
+            MessageBox.Show("Самый лучший мишка!Выбор сделан!", "Сообщение");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            poligon= new GamePoligon();
+            poligon.SetTypeBear(typeBear);
+            this.Close();
+            poligon.Show();
+        }
+
+       
+    }
+}
